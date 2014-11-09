543e26307446653cb98fea18dccc8ce380d70aa6
Dana Scheider <dana.scheider@gmail.com>
2014-11-09 00:12:49 -0800

    Connect to Database with Sequel

--- lib/test_app.rb
+++ lib/test_app.rb
@@ -5,6 +5,8 @@ require 'sequel'
 class TestApp < Sinatra::Base
   set :database, "mysql2://travis@127.0.0.1:3306/test"
 
+  DB = Sequel.connect(database)
+
   get '/index' do
     'Hello world'
   end
